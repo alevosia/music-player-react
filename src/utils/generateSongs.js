fe0b367bc75593c6a5746334db027cd5c265e500
Alexander Paul Marinas <alexanderpaul.marinas@gmail.com>
2020-11-23 21:03:30 +0800

    Move Beaver Creek

--- src/utils/generateSongs.js
+++ src/utils/generateSongs.js
@@ -2,15 +2,6 @@ import { v4 as uuidv4 } from 'uuid'
 
 export const generateSongs = () => [
     {
-        name: 'Beaver Creek',
-        cover:
-            'https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg',
-        artist: 'Aso, Middle School, Aviino',
-        audio: 'https://mp3.chillhop.com/serve.php/?mp3=10075',
-        color: ['#205950', '#2ab3bf'],
-        id: uuidv4(),
-    },
-    {
         name: 'Daylight',
         cover:
             'https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg',
@@ -26,6 +17,15 @@ export const generateSongs = () => [
         artist: 'Swørn',
         audio: 'https://mp3.chillhop.com/serve.php/?mp3=9222',
         color: ['#CD607D', '#c94043'],
+        id: uuidv4(),
+    },
+    {
+        name: 'Beaver Creek',
+        cover:
+            'https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg',
+        artist: 'Aso, Middle School, Aviino',
+        audio: 'https://mp3.chillhop.com/serve.php/?mp3=10075',
+        color: ['#205950', '#2ab3bf'],
         id: uuidv4(),
     },
     {
