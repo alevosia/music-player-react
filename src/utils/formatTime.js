18b337277b6480b7560b44a67ae28896c862a60e
Alexander Paul Marinas <alexanderpaul.marinas@gmail.com>
2020-11-23 20:56:24 +0800

    Simplify formatTime

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,19 +1,10 @@
 export function formatTime(seconds) {
-    let hours, minutes
-    let string = ''
-
-    seconds = Math.floor(seconds)
-
-    hours = Math.floor(seconds / 3600)
-    minutes = Math.floor((seconds % 3600) / 60)
-    seconds = Math.floor(seconds % 60)
-
-    if (hours) {
-        string += hours > 9 ? hours : `0${hours}:`
+    if (typeof seconds !== 'number' || isNaN(seconds)) {
+        return '0:00'
     }
 
-    string += minutes > 9 ? minutes : `0${minutes}:`
-    string += seconds > 9 ? seconds : `0${seconds}`
+    const minutes = Math.floor(seconds / 60)
+    seconds = `0${Math.floor(seconds % 60)}`.slice(-2)
 
-    return string
+    return `${minutes}:${seconds}`
 }
