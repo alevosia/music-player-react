6414443021b4a8f9e9669ddbf2553088607fdde6
Alexander Paul Marinas <alexanderpaul.marinas@gmail.com>
2020-11-23 18:36:03 +0800

    Update range slider on time update

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -9,6 +9,7 @@ export const Player = ({
     nextSong,
 }) => {
     const audioRef = useRef()
+    const rangeRef = useRef()
 
     function toggleSong() {
         if (audioRef.current) {
@@ -22,17 +23,39 @@ export const Player = ({
         }
     }
 
+    function updateRange(event) {
+        if (rangeRef.current) {
+            const { duration, currentTime } = event.target
+            rangeRef.current.value = (currentTime / duration) * 100
+        }
+    }
+
+    // Whenever the current song changes and the status is playing
+    // automatically play the new song
     useEffect(() => {
         if (status === 'PLAYING' && audioRef.current.paused) {
             audioRef.current.play()
         }
     }, [status, currentSong])
 
+    // Whenever the current song changes, set the range's value to zero
+    useEffect(() => {
+        if (rangeRef.current) {
+            rangeRef.current.value = 0
+        }
+    }, [currentSong])
+
     return (
         <div className="player">
             <div className="time-control">
                 <p>Start Time</p>
-                <input type="range" />
+                <input
+                    type="range"
+                    min="0"
+                    max="100"
+                    defaultValue="0"
+                    ref={rangeRef}
+                />
                 <p>End Time</p>
             </div>
             <div className="play-control">
@@ -44,7 +67,12 @@ export const Player = ({
                 )}
                 <FaStepForward onClick={nextSong} />
             </div>
-            <audio ref={audioRef} src={currentSong.audio} />
+            <audio
+                ref={audioRef}
+                src={currentSong.audio}
+                onEnded={nextSong}
+                onTimeUpdate={updateRange}
+            />
         </div>
     )
 }
