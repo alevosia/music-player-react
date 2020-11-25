2ef988b548d4fef7d7656c9a0fd9e6405574d647
Alexander Paul Marinas <alexanderpaul.marinas@gmail.com>
2020-11-25 20:39:44 +0800

    Add useIsFirstRender hook

---
+++ src/hooks/useIsFirstRender.js
@@ -0,0 +1,11 @@
+import { useEffect, useRef } from 'react'
+
+export const useIsFirstRender = () => {
+    const isFirstRenderRef = useRef(true)
+
+    useEffect(() => {
+        isFirstRenderRef.current = false
+    }, [])
+
+    return isFirstRenderRef.current
+}
